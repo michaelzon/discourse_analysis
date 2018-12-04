90cd69984531b2ae02a9bb282e14f205c7143fa7
michaelzon <michael.zonneveld@live.nl>
2018-12-04 14:30:01 +0100

    building a better dataview for analysis

--- article_list_test.py
+++ article_list_test.py
@@ -1,1 +1,4 @@
-article_list_test = [ "ad_test/ad_1998.txt", "ad_test/ad_1999.txt", "ad_test/ad_2000.txt", "ad_test/ad_2001.txt", "ad_test/ad_2002.txt"]
+article_list_test = [ "ad_test/ad_1998.txt", "ad_test/ad_1999.txt", "ad_test/ad_2000.txt", "ad_test/ad_2001.txt", "ad_test/ad_2002.txt",
+"ad_test/telegraaf_1998.txt", "ad_test/telegraaf_1999.txt", "ad_test/telegraaf_2000.txt", "ad_test/telegraaf_2001.txt", "ad_test/telegraaf_2002.txt",
+"ad_test/volkskrant_1998.txt", "ad_test/volkskrant_1999.txt", "ad_test/volkskrant_2000.txt", "ad_test/volkskrant_2001.txt", "ad_test/volkskrant_2002.txt",
+"ad_test/nrc_1998.txt", "ad_test/nrc_1999.txt", "ad_test/nrc_2000.txt", "ad_test/nrc_2001.txt", "ad_test/nrc_2002.txt"]
