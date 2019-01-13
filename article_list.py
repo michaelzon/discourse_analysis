c432e6bd3e6ab41e0d7f6f9295f12e0ba0f5578b
michaelzon <michael.zonneveld@live.nl>
2019-01-13 17:32:10 +0100

    removed inrelevant comments

--- article_list.py
+++ article_list.py
@@ -1,6 +1,3 @@
-# article_list = [ "ad_test/ad_1998.txt" ]
-#  # "ad_test/ad_1999.txt", "ad_test/ad_2000.txt", "ad_test/ad_2001.txt", "ad_test/ad_2002.txt"]
-
 article_list = [
 "samples/AD_Algemeen_Dagblad_1998.TXT" ,
 "samples/AD_Algemeen_Dagblad_1999.TXT" ,
